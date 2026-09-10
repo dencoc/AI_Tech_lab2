0b7ce447bf4620ab9a59cfd1fd1fb34b06eff277
dencoc <denoff39@email.com>
2026-09-10 19:41:02 +0300

    feat: Добавлен подсчет накопительной скидки для магазина быта

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,47 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_purchase_count(self):
+        return self.purchase_set.count()
+
+    def get_discount(self):
+        purchase_count = self.get_purchase_count()
+
+        if purchase_count >= 20:
+            return 15
+        elif purchase_count >= 10:
+            return 10
+        elif purchase_count >= 5:
+            return 5
+
+        return 0
+
+
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    person = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+
+    def get_discount(self):
+        return self.customer.get_discount()
+
+    def get_price_with_discount(self):
+        discount = self.get_discount()
+        return self.product.price * (100 - discount) // 100
+
+    def __str__(self):
+        return f'{self.customer} — {self.product}'
--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic import View
 
-from .models import Product, Purchase
+from .models import Product, Customer, Purchase
 
-# Create your views here.
+
 def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'shop/index.html', context)
 
 
-class PurchaseCreate(CreateView):
-    model = Purchase
-    fields = ['product', 'person', 'address']
+class PurchaseCreate(View):
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
 
-    def form_valid(self, form):
-        self.object = form.save()
-        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
+        return render(request, 'shop/buy.html', {
+            'product': product
+        })
 
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+
+        customer, created = Customer.objects.get_or_create(
+            name=name,
+            address=address
+        )
+
+        discount = customer.get_discount()
+
+        price = product.price
+        final_price = price * (100 - discount) // 100
+
+        Purchase.objects.create(
+            product=product,
+            customer=customer
+        )
+
+        return HttpResponse(
+            f'Спасибо за покупку, {customer.name}! '
+            f'Скидка: {discount}%. '
+            f'К оплате: {final_price} руб.'
+        )
